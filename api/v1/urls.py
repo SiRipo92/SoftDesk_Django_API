cd1205b7e35c0eafc13c046d76028e1ad5635c82
SiRipo92 <ripochesierra@gmail.com>
2025-12-28 18:44:07 +0100

    Removed DRF Spectacular imports and urls from api/v1/urls.py

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -6,13 +6,8 @@ car elles sont incluses par config/urls.py.
 """
 
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     # Module Users (signup/login + refresh/logout + me)
     path("", include(("apps.users.urls", "users"), namespace="users")),
-
-    # API docs
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
 ]
