4cb7f1c43ce46036264074147d4edb64dcf24a33
SiRipo92 <ripochesierra@gmail.com>
2026-02-05 11:56:39 +0100

    Reduced payload of pagination from 20 to 10 for easier demo in soutenance

--- common/paginator.py
+++ common/paginator.py
@@ -16,9 +16,9 @@ class DefaultPagination(PageNumberPagination):
 
     Query params:
         - ?page=1
-        - ?page_size=20 (optional, capped)
+        - ?page_size=10 (optional, capped)
     """
 
-    page_size = 20
+    page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 100
